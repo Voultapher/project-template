eabd70974f514d317369a1b513629ea508adcc25
Voultapher <lukas.bergdoll@gmail.com>
2017-01-26 15:49:04 +0100

    First commit

---
+++ create.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ $# != 2 ]; then
+    echo 'Invalid parameter count. Provide "Name" "Destination"'
+    exit -1;
+fi
+
+ProjectName=$1
+Destination=$(readlink -f $2)
+Source='template/.'
+
+cp -r $Source $Destination
+
+# configure atom-build-tools
+BT_FILE=$Destination/.build-tools.cson
+sed -i.bck -e 's|PROJECT_NAME|'$ProjectName'|' $BT_FILE
+sed -i.bck -e 's|project: ""|project: "'$Destination'"|' $BT_FILE
+sed -i.bck -e 's|source: ""|source: "'$BT_FILE'"|' $BT_FILE
+
+rm $BT_FILE.bck
+rm $BT_FILE.swp
+
+# configure CMake
+CM_FILE=$Destination/CMakeLists.txt
+sed -i.bck -e 's|project (PROJECT_NAME)|project ('$ProjectName')|' $CM_FILE
+
+rm $CM_FILE.bck
+
+echo 'Created project "'$ProjectName'" at location "'$Destination'"'
