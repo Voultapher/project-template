a32b2114ac4da34db88ecb4152681bad79bff5ba
Voultapher <lukas.bergdoll@gmail.com>
2017-01-26 19:09:55 +0100

    Improved test

--- test/main.sh
+++ test/main.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[1;32m'
+NC='\033[0m' # No Color
+
+function cleanup()
+{
+    rm -rf $TestDir
+}
+
+function check_exit_code()
+{
+    retval=$?
+    if [ $retval != 0 ]; then
+        printf "${RED}Error${NC}: script returned: $retval\n"
+        cleanup
+        exit -1;
+    fi
+}
+
 ScriptDir=$(readlink -f '../')
 ScriptName='create.sh'
 
@@ -8,9 +27,15 @@ TestDir='../../PT_Test_Dir'
 
 rm -rf $TestDir
 
+
 cd $ScriptDir
+
+# Run script
 ./$ScriptName $TestName $TestDir
+check_exit_code
 
 
-echo 'all tests passed'
+cleanup
+
+printf "${GREEN}Success${NC}: all tests passed\n"
 exit 0
