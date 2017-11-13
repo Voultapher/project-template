2b079e3875a80aa84f68ad53201d53abed53d646
Lukas Bergdoll <lukas.bergdoll@gmail.com>
2017-11-13 19:39:01 +0100

    Improve default config

--- template/configure.sh
+++ template/configure.sh
@@ -8,6 +8,7 @@ SCRIPT_PATH=$(dirname ${SCRIPT_FILE})
 mkdir -p "${SCRIPT_PATH}/Debug"
 cd "${SCRIPT_PATH}/Debug"
 cmake -G Ninja \
+  -D CMAKE_C_COMPILER=clang \
   -D CMAKE_CXX_COMPILER=clang++ \
   -D CMAKE_CXX_FLAGS="-Wall -Wextra -stdlib=libc++ -fno-rtti -fsanitize=address,undefined -fno-omit-frame-pointer" \
   -D CMAKE_BUILD_TYPE=Debug \
@@ -16,7 +17,11 @@ cmake -G Ninja \
 mkdir -p "${SCRIPT_PATH}/Release"
 cd "${SCRIPT_PATH}/Release"
 cmake -G Ninja \
+  -D CMAKE_C_COMPILER=clang \
   -D CMAKE_CXX_COMPILER=clang++ \
-  -D CMAKE_CXX_FLAGS="-stdlib=libc++ -fno-rtti -flto=thin -fuse-ld=lld" \
+  -D CMAKE_CXX_FLAGS="-stdlib=libc++ -fno-rtti -flto=thin" \
+  -D CMAKE_STATIC_LINKER_FLAGS="-fuse-ld=lld" \
+  -D CMAKE_SHARED_LINKER_FLAGS="-fuse-ld=lld" \
+  -D CMAKE_MODULE_LINKER_FLAGS="-fuse-ld=lld" \
   -D CMAKE_BUILD_TYPE=Release \
   -D CMAKE_EXPORT_COMPILE_COMMANDS=1 ..
