d5f537564a6629f302e559a66c500023d38092ae
Lukas Bergdoll <lukas.bergdoll@gmail.com>
2018-02-11 10:28:33 +0100

    Add color diagnostic to build

--- template/configure.sh
+++ template/configure.sh
@@ -10,7 +10,7 @@ cd "${SCRIPT_PATH}/Debug"
 cmake -G Ninja \
   -D CMAKE_C_COMPILER=clang \
   -D CMAKE_CXX_COMPILER=clang++ \
-  -D CMAKE_CXX_FLAGS="-Wall -Wextra -stdlib=libc++ -fno-rtti -fsanitize=address,undefined -fno-omit-frame-pointer" \
+  -D CMAKE_CXX_FLAGS="-Wall -Wextra -stdlib=libc++ -fno-rtti -fsanitize=address,undefined -fno-omit-frame-pointer -fcolor-diagnostics" \
   -D CMAKE_BUILD_TYPE=Debug \
   -D CMAKE_EXPORT_COMPILE_COMMANDS=1 ..
 
@@ -19,9 +19,10 @@ cd "${SCRIPT_PATH}/Release"
 cmake -G Ninja \
   -D CMAKE_C_COMPILER=clang \
   -D CMAKE_CXX_COMPILER=clang++ \
-  -D CMAKE_CXX_FLAGS="-stdlib=libc++ -fno-rtti -flto=thin" \
-  -D CMAKE_STATIC_LINKER_FLAGS="-fuse-ld=lld" \
-  -D CMAKE_SHARED_LINKER_FLAGS="-fuse-ld=lld" \
+  -D CMAKE_CXX_FLAGS="-stdlib=libc++ -fno-rtti -flto=thin -fcolor-diagnostics" \
+  -D CMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld" \
   -D CMAKE_MODULE_LINKER_FLAGS="-fuse-ld=lld" \
+  -D CMAKE_SHARED_LINKER_FLAGS="-fuse-ld=lld" \
+  -D CMAKE_STATIC_LINKER_FLAGS="-fuse-ld=lld" \
   -D CMAKE_BUILD_TYPE=Release \
   -D CMAKE_EXPORT_COMPILE_COMMANDS=1 ..
