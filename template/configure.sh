efa6b5b6e5e87a3e44daaccc2b192a00ccb89748
Lukas Bergdoll <lukas.bergdoll@gmail.com>
2018-10-21 20:10:12 +0200

    Remove lld as static linker
    Set lld as default for both debug and release

--- template/configure.sh
+++ template/configure.sh
@@ -11,6 +11,9 @@ cmake -G Ninja \
   -D CMAKE_C_COMPILER=clang \
   -D CMAKE_CXX_COMPILER=clang++ \
   -D CMAKE_CXX_FLAGS="-Wall -Wextra -stdlib=libc++ -fno-rtti -fsanitize=address,undefined -fno-omit-frame-pointer -fcolor-diagnostics" \
+  -D CMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld" \
+  -D CMAKE_MODULE_LINKER_FLAGS="-fuse-ld=lld" \
+  -D CMAKE_SHARED_LINKER_FLAGS="-fuse-ld=lld" \
   -D CMAKE_BUILD_TYPE=Debug \
   -D CMAKE_EXPORT_COMPILE_COMMANDS=1 ..
 
@@ -23,6 +26,5 @@ cmake -G Ninja \
   -D CMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld" \
   -D CMAKE_MODULE_LINKER_FLAGS="-fuse-ld=lld" \
   -D CMAKE_SHARED_LINKER_FLAGS="-fuse-ld=lld" \
-  -D CMAKE_STATIC_LINKER_FLAGS="-fuse-ld=lld" \
   -D CMAKE_BUILD_TYPE=Release \
   -D CMAKE_EXPORT_COMPILE_COMMANDS=1 ..
